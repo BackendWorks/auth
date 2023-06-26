75031824a97cb12f11d5cf89f72033647e3fae59
harsh-simform <harsh.m@simformsolutions.com>
2023-06-27 00:19:27 +0530

    fix: firebase config build issues

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import * as firebase from 'firebase-admin';
-import * as firebaseConfig from '../../firebase.config.json';
+// import * as firebaseConfig from '../../firebase.config.json';
 
 @Injectable()
 export class FirebaseService {
   private firebaseAdmin: firebase.app.App;
   constructor() {
-    const params = {
-      type: firebaseConfig.type,
-      projectId: firebaseConfig.project_id,
-      privateKeyId: firebaseConfig.private_key_id,
-      privateKey: firebaseConfig.private_key,
-      clientEmail: firebaseConfig.client_email,
-      clientId: firebaseConfig.client_id,
-      authUri: firebaseConfig.auth_uri,
-      tokenUri: firebaseConfig.token_uri,
-      authProviderX509CertUrl: firebaseConfig.auth_provider_x509_cert_url,
-      clientC509CertUrl: firebaseConfig.client_x509_cert_url,
-    };
+    // const params = {
+    //   type: firebaseConfig.type,
+    //   projectId: firebaseConfig.project_id,
+    //   privateKeyId: firebaseConfig.private_key_id,
+    //   privateKey: firebaseConfig.private_key,
+    //   clientEmail: firebaseConfig.client_email,
+    //   clientId: firebaseConfig.client_id,
+    //   authUri: firebaseConfig.auth_uri,
+    //   tokenUri: firebaseConfig.token_uri,
+    //   authProviderX509CertUrl: firebaseConfig.auth_provider_x509_cert_url,
+    //   clientC509CertUrl: firebaseConfig.client_x509_cert_url,
+    // };
     this.firebaseAdmin = firebase.initializeApp({
       credential: firebase.credential.cert(params),
     });
