c739a29ad602fac777c619bb725d69cdde4e1273
rodionros <r.rostovshchikov@yandex.ru>
2025-01-10 10:40:57 +0300

    feat: add auction and chat submodule

--- src/modules/company/controllers/company.auth.controller.ts
+++ src/modules/company/controllers/company.auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
 
